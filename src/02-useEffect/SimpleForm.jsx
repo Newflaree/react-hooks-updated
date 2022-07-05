f62ad2991f03532efb063b09ca4f7b1924de0466
Camilo López <camiloj.lopez79@gmail.com>
2022-07-05 12:16:40 -0400

    useEffect - SimpleForm

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+
+export const SimpleForm = () => {
+  const [ formState, setFormState ] = useState({
+    username: 'Test',
+    email: 'test@email.com' 
+  });
+
+  const { username, email } = formState;
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+
+    setFormState({
+      ...formState,
+      [ name ]: value
+    });
+  }
+
+  useEffect( () => {
+    console.log( 'useEffect callled' );
+  });
+
+  return (
+    <>
+      <h1>Simple Form</h1>
+      <hr />
+
+      <input 
+        type='text'
+        className='form-control mb-2'
+        placeholder='Username'
+        name='username'
+        value={ username }
+        onChange={ onInputChange }
+      /> 
+
+      <input 
+        type='email'
+        className='form-control mb-2'
+        placeholder='Email'
+        name='email'
+        value={ email }
+        onChange={ onInputChange }
+      /> 
+    </>
+  )
+}
