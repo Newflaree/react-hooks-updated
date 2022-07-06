9e784ecf67dbc1cb86824221c325e11428e5f8fa
Camilo López <camiloj.lopez79@gmail.com>
2022-07-06 02:43:34 -0400

    useFetch + useCounter

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -4,13 +4,34 @@ const url = 'https://www.breakingbadapi.com/api/quotes/1';
 
 export const MultipleCustomHook = () => {
   const { data, isLoading, hasError } = useFetch( url );
-
-  console.log( data, isLoading, hasError );
+  const { author, quote } = !!data && data[0];
 
   return (
     <>
       <h1>BreakingBad Quotes</h1>
       <hr />
+
+      {
+        isLoading
+
+          ? (
+            <div className='alert alert-info text-center'>
+              Loading...
+            </div> 
+          )
+
+          : (
+            <blockquote className='blockquote text-end'>
+              <p className='mb-1'>{ quote }</p>
+              <footer className='blockquote-footer mt-1'> { author }</footer>
+            </blockquote>
+          )
+      }
+
+      <button className='btn btn-primary'>
+        Next quote
+      </button>
+
     </>
   );
 }
