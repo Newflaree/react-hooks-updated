628f5ad5e79d9311f1199eb78e780512f9791875
Camilo López <camiloj.lopez79@gmail.com>
2022-07-06 13:31:18 -0400

    useLayoutEffect

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,8 +1,26 @@
+import { useLayoutEffect, useRef, useState } from 'react';
+
 export const Quote = ({ quote, author }) => {
+  const pRef = useRef();
+
+  const [ boxSize, setBoxSize ] = useState({ width: 0, height: 0 });
+
+  useLayoutEffect( () => {
+    const { width, height } = pRef.current.getBoundingClientRect();
+    setBoxSize({ width, height });
+  }, [ quote ]);
+
   return (
-    <blockquote className='blockquote text-end'>
-      <p className='mb-1'>{ quote }</p>
-      <footer className='blockquote-footer mt-1'> { author }</footer>
-    </blockquote>
+    <>
+      <blockquote 
+        className='blockquote text-end'
+        style={{ display: 'flex' }}
+      >
+        <p className='mb-1' ref={ pRef }>{ quote }</p>
+        <footer className='blockquote-footer mt-1'> { author }</footer>
+      </blockquote>
+
+      <code>{ JSON.stringify( boxSize ) }</code>
+    </>
   );
 }
